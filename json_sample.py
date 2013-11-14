032ce66c05ef1131eab4b8e715551204ebc9e0d6
washingtonzju <zhongkai.mi@gmail.com>
2013-11-14 17:37:04 +0800

    Update json_sample.py

--- json_sample.py
+++ json_sample.py
@@ -1,4 +1,9 @@
 def parsing_log(data, f_name, user_name):
+    '''
+    json.load() method convert string like [{a:b},{c:d}] into respongding
+    object: a list whose item is a dictionary. It depends on the contents
+    structure of the json source.
+    '''
     data_lst = json.loads(data.decode("utf-8-sig"))
     csv_file = codecs.open(f_name, "a+", encoding="utf-8")
     for log in data_lst:

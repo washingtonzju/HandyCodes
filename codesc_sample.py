04c2863df629f13c7c66253a43d29517b7b75c61
washingtonzju <zhongkai.mi@gmail.com>
2013-11-14 17:43:23 +0800

    Update codesc_sample.py

--- codesc_sample.py
+++ codesc_sample.py
@@ -2,6 +2,10 @@ import json
 import codesc
 
 def gen_titles(data, f_name, user_name):
+    '''
+    codecs is a class that solve character issues.
+    codecs.open() is a method that replace open(). It provides character supports.
+    '''
     data_lst = json.loads(data.decode("utf-8-sig"))
 
     csv_file = codecs.open(f_name, "w", encoding="utf-8")
